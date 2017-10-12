cba3d363c74b96a2ca5daabc0aec1a1b02d960a0
Kenji Mukai <kensuka21@gmail.com>
2017-10-12 08:35:30 -0400

    Removing default template and adding some styles.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xs-6 text-center">
+              <input className="" type="text"/>
+          </div>
+
+          <div className="separator"></div>
+
+          <div className="col-xs-6 text-center">
+              <input className="" type="text"/>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
