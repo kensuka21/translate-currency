7ab56285f6b6fd38bde35d6452c6a7dacf61d13d
Kenji Mukai <kensuka21@gmail.com>
2017-10-14 10:23:02 -0400

    Adding flag images.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,68 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import dominicanFlag from './assets/images/dominican.jpg';
+import usaFlag from './assets/images/usa.jpg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-xs-6 text-center">
-              <div className="row input-currency">
-                  <div className="col-xs-4"></div>
-                  <div className="col-xs-4">
-                      <input className="form-control" type="text"/>
-                  </div>
-                  <div className="col-xs-4"></div>
-              </div>
-          </div>
+    constructor(props) {
+        super(props);
 
-          <div className="separator"></div>
+        this.state = {
+        };
+    }
 
-          <div className="col-xs-6 text-center">
-              <div className="row">
-                  <div className="col-xs-4"></div>
-                  <div className="col-xs-4">
-                      <input className="form-control" type="text"/>
-                  </div>
-                  <div className="col-xs-4"></div>
-              </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="header">
+                    <h2 className="text-center">CURRENCY TRANSLATOR</h2>
+                </div>
+                <div className="row">
+
+                    <div className="col-xs-5 text-center">
+                        <div className="vertical-center">
+                            <div className="row ">
+                                <div className="col-xs-5"></div>
+                                <div className="col-xs-4 ">
+                                    <img className="img img-thumbnail" src={dominicanFlag} width="200" height="95"/>
+                                    <div className="input-group">
+                                        <span className="input-group-addon">$</span>
+                                        <input type="number" step="any" className="form-control"
+                                               placeholder="DOP"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-xs-2 text-center ">
+                        <div className="top-separator"></div>
+                        <div className="btn-vertical-center">
+                            <button className="btn btn-primary">Translate</button>
+                        </div>
+                        <div className="bottom-separator"></div>
+                    </div>
+
+                    <div className="col-xs-5  text-center">
+                        <div className="vertical-center">
+                            <div className="row ">
+                                <div className="col-xs-3"></div>
+                                <div className="col-xs-4 ">
+                                    <img className="img img-thumbnail" src={usaFlag} width="200" height="95"/>
+                                    <div className="input-group">
+                                        <span className="input-group-addon">$</span>
+                                        <input readOnly type="text" className="form-control"
+                                               placeholder="USD"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
